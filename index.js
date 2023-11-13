b5eb30fa16842d13991107f4ee41b6c795173e9b
RajMahammadAli <ali.rc.raj@gmail.com>
2023-11-13 15:31:54 +0600

    server is connected to mongodb

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const uri =
   "mongodb+srv://alircraj:BsgGA8mawEIzuS3w@cluster0.gu1mcti.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
